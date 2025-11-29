50252e1eb5367fe1ccfaeb513ceaf62e86588426
zahra shahbazi <zahra.shahbazi.nafas@gmail.com>
2025-11-29 19:34:57 +0330

    learnin booleans

---
+++ booleans.py
@@ -0,0 +1,12 @@
+pasword=input(' ramz obor ra vard konid  ')
+
+confirm=input('  tekrar ramz obor ')
+
+if pasword==confirm:
+
+
+          print("ramz obor shoma ba movafaghiat taeed shod")
+
+else:
+
+       print('ramz eshtebah ast')
